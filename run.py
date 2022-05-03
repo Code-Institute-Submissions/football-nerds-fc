8a6c5dacf0c58d6bd4b89a679765a9a31b5684b6
PedroMiguelFerreira <pedro.miguel.pinto.ferreira@gmail.com>
2022-05-03 00:08:01 +0000

    Add info to docstring for the question class

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ class FootballQuizQuestion:
     Model a real-world entity - in this case, and
     because it's a quiz, it's a data type/class for a
     question.
-    Stores the question's prompt and the answer, all
-    the information we need to create a question.
+    Stores the question's prompt and the answer, its
+    attributes - all the information we need to create
+    a question.
     """
     def __init__(self, prompt, answer):
         self.prompt = prompt
